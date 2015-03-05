aaa752017da323e0fb138693fedf52eff430acc8
wlaurance <w.laurance@gmail.com>
2015-03-05 14:48:30 -0800

    Clean up the old app js fingerprinted

--- tasks/browserify-app.js
+++ tasks/browserify-app.js
@@ -15,6 +15,7 @@ var sourcemaps = require('gulp-sourcemaps');
 var source = require('vinyl-source-stream');
 var prettyHrtime = require('pretty-hrtime');
 var ngAnnotate = require('gulp-ng-annotate');
+var rimraf = require('gulp-rimraf');
 
 gulp.task('browserify:app', function () {
   var bundler = browserify(config.browserify);
@@ -27,16 +28,16 @@ gulp.task('browserify:app', function () {
   function bundle () {
     gutil.log('Bundling ' + gutil.colors.magenta(config.output) + '...');
     var start = process.hrtime();
-    return bundler
-      .bundle()
+    return gulp.src(config.dest + '/app**.js*')
+      .pipe(rimraf({force: true}))
+      .pipe(bundler.bundle())
       .pipe(source(config.output))
       .pipe(buffer())
-      .pipe(gulpif(env.isDev(), rename({suffix: '.min'})))
-      .pipe(gulpif(env.isDev(), sourcemaps.init({loadMaps: true})))
+      .pipe(gulpif(env.isDev(), sourcemaps.init()))
       .pipe(ngAnnotate())
-      .pipe(uglify({mangle: false}))
-      .pipe(gulpif(env.isDev(), sourcemaps.write('./')))
       .pipe(rev())
+      .pipe(gulpif(env.isProd(), uglify({mangle: false})))
+      .pipe(sourcemaps.write('./'))
       .pipe(gulpif(env.isProd(), rename({suffix: '.min'})))
       .pipe(gulp.dest(config.dest))
       .on('end', function () {
@@ -44,6 +45,7 @@ gulp.task('browserify:app', function () {
         var prettyTime = prettyHrtime(end);
         gutil.log('Bundled ' + gutil.colors.magenta(config.output) + ' after ' + gutil.colors.magenta(prettyTime));
       });
+
   }
 
   return bundle();
